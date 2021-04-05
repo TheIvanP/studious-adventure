20064861322e85cbfb9bc15bdbf3e94235e53da1
Ivan Pedersen <ivanpedersen79@gmail.com>
2021-04-05 23:31:57 +0100

    move functions to helpers, implement queries

---
+++ helpers.py
@@ -0,0 +1,212 @@
+# Import Python packages
+import os
+import glob
+import csv
+import black
+
+
+def process_files():
+    # #### Creating list of filepaths to process original event csv data files
+    # In[2]:
+    # checking your current working directory
+    print(os.getcwd())
+
+    # Get your current folder and subfolder event data
+    filepath = os.getcwd() + "/event_data"
+
+    # Create a for loop to create a list of files and collect each filepath
+    for root, dirs, files in os.walk(filepath):
+
+        # join the file path and roots with the subdirectories using glob
+        file_path_list = glob.glob(os.path.join(root, "*"))
+        # print(file_path_list)
+
+    # #### Processing the files to create the data file csv that will be used for Apache Casssandra tables
+    # initiating an empty list of rows that will be generated from each file
+    full_data_rows_list = []
+
+    # for every filepath in the file path list
+    for f in file_path_list:
+
+        # reading csv file
+        with open(f, "r", encoding="utf8", newline="") as csvfile:
+            # creating a csv reader object
+            csvreader = csv.reader(csvfile)
+            next(csvreader)
+
+            # extracting each data row one by one and append it
+            for line in csvreader:
+                # print(line)
+                full_data_rows_list.append(line)
+
+    # uncomment the code below if you would like to get total number of rows
+    # print(len(full_data_rows_list))
+    # uncomment the code below if you would like to check to see what the list of event data rows will look like
+    # print(full_data_rows_list)
+
+    # creating a smaller event data csv file called event_datafile_full csv that will be used to insert data into the \
+    # Apache Cassandra tables
+    csv.register_dialect("myDialect", quoting=csv.QUOTE_ALL, skipinitialspace=True)
+
+    with open("event_datafile_new.csv", "w", encoding="utf8", newline="") as f:
+        writer = csv.writer(f, dialect="myDialect")
+        writer.writerow(
+            [
+                "artist",
+                "firstName",
+                "gender",
+                "itemInSession",
+                "lastName",
+                "length",
+                "level",
+                "location",
+                "sessionId",
+                "song",
+                "userId",
+            ]
+        )
+        for row in full_data_rows_list:
+            if row[0] == "":
+                continue
+            writer.writerow(
+                (
+                    row[0],
+                    row[2],
+                    row[3],
+                    row[4],
+                    row[5],
+                    row[6],
+                    row[7],
+                    row[8],
+                    row[12],
+                    row[13],
+                    row[16],
+                )
+            )
+
+    # check the number of rows in your csv file
+    with open("event_datafile_new.csv", "r", encoding="utf8") as f:
+        print(sum(1 for line in f))
+
+
+#%%
+def test_query(query_in: str, session, should_limit=False, limit=5) -> None:
+    """reads a query string, executes it on the given session
+    and print each returned row using black formatter
+
+    Args:
+    query_in:str        query string expecting cassandra query language
+    session:session     cassandra session object
+    should_limit:bool   toggle for limiting number of returned rows
+    limit:int           how many rows to return if limiting
+    """
+
+    query = query_in
+    if should_limit:
+        query = f"{query_in} LIMIT {limit}"
+
+    try:
+        rows = session.execute(query)
+    except Exception as e:
+        print(e)
+
+    # print(f"Input Query: {query_in}")
+    print("-" * 50)
+    print("Input Query: ")
+    print(" " * 50)
+    print(query_in)
+    print("-" * 50)
+    print("Result: ")
+    print(" " * 50)
+    for row in rows:
+        print(black.format_str(repr(row), mode=black.Mode()))
+    print(" " * 50)
+    print("-" * 50)
+
+
+def drop_table(tables: tuple, session) -> None:
+    for table in tables:
+        query = f"DROP TABLE IF EXISTS {table}"
+        try:
+            session.execute(query)
+        except Exception as e:
+            print(e)
+
+
+def insert_music_library_col(
+    row, query_cols: tuple, table_name: str, session, col_name_file_map: dict
+) -> None:
+    """Construct query to insert row of data from pandas itetuple object using
+    list of columns into the named table and execute it with the
+    given cassandra session
+
+    Args:
+            row:pandas object   row from pandas itertuple method
+            query_cols:tuple    list of column names to insert
+            table_name:str      name of the table to insert into
+            session             cassandra session object
+
+    Returns:
+            None
+
+    """
+
+    def _get_value_from_csv(row, key: str, col_name_file_map: dict):
+        """Takes a row and extracts the value
+        using the defined column mapping
+
+        This saves us from having to deal with
+        column names as defined in the csv, we can
+        stick with our own naming convention
+
+        Args:
+        row:Row casssandra row object
+        key:str column name to get as defined in table names
+        """
+
+        return row._asdict().get(col_name_file_map.get(key))
+
+    #  get elements in query_cols as one string for insertion into query
+    query_cols_asstring = ", ".join(query_cols)
+
+    #  compose query insert statement
+    query_insert = f"INSERT INTO {table_name} ({query_cols_asstring}) "
+
+    #  for each element in query cols create value substitution magics
+    subs = ", ".join(["%s" for x in query_cols])
+
+    #  compose value insertion query string
+    values = f"VALUES ({subs})"
+
+    #  get the data from row looking up the column names
+    cols = [
+        _get_value_from_csv(row, col_name, col_name_file_map) for col_name in query_cols
+    ]
+
+    # execute the session, casting cols to tuple for compatability with execute method
+    try:
+        session.execute(
+            f"{query_insert}{values}",
+            tuple(cols),
+        )
+    except Exception as e:
+        print(e)
+
+
+def create_table(table_name: str, session, table_create_mapper: dict) -> None:
+    """Compose a query for creating a table based on table name and
+    looking up query based on table name in table_name_create_mapper
+
+    Args:
+    table_name:str      name of the table to insert. also used for looking up query
+    session:            cassandra session
+    table_create_mapper:dict with names of tables to insert and queries for cols and primary keys
+    """
+
+    query = (
+        f"CREATE TABLE IF NOT EXISTS {table_name} {table_create_mapper.get(table_name)}"
+    )
+    try:
+        session.execute(query)
+    except Exception as e:
+        print(e)
--- musiclib_dbman.py
+++ musiclib_dbman.py
@@ -1,161 +1,29 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Part I. ETL Pipeline for Pre-Processing the Files
-
-# ## PLEASE RUN THE FOLLOWING CODE FOR PRE-PROCESSING THE FILES
-
-# #### Import Python packages
-
-# In[1]:
-
-
-# Import Python packages
-import re
-import os
-import glob
-import json
-import csv
-
-import numpy as np
 import pandas as pd
-import cassandra
-
-
-# #### Creating list of filepaths to process original event csv data files
-
-# In[2]:
-# checking your current working directory
-print(os.getcwd())
-
-# Get your current folder and subfolder event data
-filepath = os.getcwd() + "/event_data"
-
-# Create a for loop to create a list of files and collect each filepath
-for root, dirs, files in os.walk(filepath):
-
-    # join the file path and roots with the subdirectories using glob
-    file_path_list = glob.glob(os.path.join(root, "*"))
-    # print(file_path_list)
-
-
-# #### Processing the files to create the data file csv that will be used for Apache Casssandra tables
-
-# In[3]:
-
-
-# initiating an empty list of rows that will be generated from each file
-full_data_rows_list = []
-
-# for every filepath in the file path list
-for f in file_path_list:
-
-    # reading csv file
-    with open(f, "r", encoding="utf8", newline="") as csvfile:
-        # creating a csv reader object
-        csvreader = csv.reader(csvfile)
-        next(csvreader)
-
-        # extracting each data row one by one and append it
-        for line in csvreader:
-            # print(line)
-            full_data_rows_list.append(line)
-
-# uncomment the code below if you would like to get total number of rows
-# print(len(full_data_rows_list))
-# uncomment the code below if you would like to check to see what the list of event data rows will look like
-# print(full_data_rows_list)
-
-# creating a smaller event data csv file called event_datafile_full csv that will be used to insert data into the \
-# Apache Cassandra tables
-csv.register_dialect("myDialect", quoting=csv.QUOTE_ALL, skipinitialspace=True)
-
-with open("event_datafile_new.csv", "w", encoding="utf8", newline="") as f:
-    writer = csv.writer(f, dialect="myDialect")
-    writer.writerow(
-        [
-            "artist",
-            "firstName",
-            "gender",
-            "itemInSession",
-            "lastName",
-            "length",
-            "level",
-            "location",
-            "sessionId",
-            "song",
-            "userId",
-        ]
-    )
-    for row in full_data_rows_list:
-        if row[0] == "":
-            continue
-        writer.writerow(
-            (
-                row[0],
-                row[2],
-                row[3],
-                row[4],
-                row[5],
-                row[6],
-                row[7],
-                row[8],
-                row[12],
-                row[13],
-                row[16],
-            )
-        )
-
-
-# In[4]:
-
-
-# check the number of rows in your csv file
-with open("event_datafile_new.csv", "r", encoding="utf8") as f:
-    print(sum(1 for line in f))
-
-
-# # Part II. Complete the Apache Cassandra coding portion of your project.
-#
-# ## Now you are ready to work with the CSV file titled <font color=red>event_datafile_new.csv</font>, located within the Workspace directory.  The event_datafile_new.csv contains the following columns:
-# - artist
-# - firstName of user
-# - gender of user
-# - item number in session
-# - last name of user
-# - length of the song
-# - level (paid or free song)
-# - location of the user
-# - sessionId
-# - song title
-# - userId
-#
-# The image below is a screenshot of what the denormalized data should appear like in the <font color=red>**event_datafile_new.csv**</font> after the code above is run:<br>
-#
-# <img src="images/image_event_datafile_new.jpg">
-
-# ## Begin writing your Apache Cassandra code in the cells below
-
-# #### Creating a Cluster
-
-# In[ ]:
-
-
-# This should make a connection to a Cassandra instance your local machine
-# (127.0.0.1)
-
 from cassandra.cluster import Cluster
 
+from helpers import (
+    drop_table,
+    insert_music_library_col,
+    process_files,
+    test_query,
+    create_table,
+)
+
+# ----------- process csv files ----------------------------------
+process_files()
+
+# ----------- set up cassandra  connection and keyspace --------------
+
+# instantiate cassandra cluster object
 cluster = Cluster()
 
-# To establish connection and begin executing queries, need a session
-session = cluster.connect()
-
+# create a session by connecting to it. assuming localhost connection
+try:
+    session = cluster.connect()
+except Exception as e:
+    print(e)
 
 # #### Create Keyspace
-
-# In[ ]:
-
 try:
     session.execute(
         """
@@ -163,95 +31,50 @@ try:
     WITH REPLICATION = 
     { 'class' : 'SimpleStrategy', 'replication_factor' : 1 }"""
     )
-
 except Exception as e:
     print(e)
 
-
 # #### Set Keyspace
-
-# In[ ]:
 try:
     session.set_keyspace("udacity")
 except Exception as e:
     print(e)
 
 #%%
-# ### Drop table before creating if if it exists
-tables = ("music_library_q1", "music_library_q2", "music_library_q3")
+# ----------- create new tables based on queries ---------------------
 
-
-def drop_table(tables: tuple) -> None:
-    for table in tables:
-        query = f"DROP TABLE IF EXISTS {table}"
-        try:
-            session.execute(query)
-        except Exception as e:
-            print(e)
-
-
-# process tables
-drop_table(tables)
 # ### Now we need to create tables to run the following queries. Remember, with Apache Cassandra you model the database tables on the queries you want to run.
-#%%
-
-#%%
-
 # ## Create queries to ask the following three questions of the data
-#
+
+# ### Drop table before creating if if it exists
+table_q1 = "music_library_q1"
+table_q2 = "music_library_q2"
+table_q3 = "music_library_q3"
+drop_table((table_q1, table_q2, table_q3), session)
+#%%
 # ### 1. Give me the artist, song title and song's length in the music app history that was heard during  sessionId = 338, and itemInSession  = 4
-query = "CREATE TABLE IF NOT EXISTS music_library_q1 "
-query = (
-    query
-    + """(artist text, song text, item_in_session int, length float, session_id int,  
-                PRIMARY KEY (session_id, item_in_session))"""
-)
-try:
-    session.execute(query)
-except Exception as e:
-    print(e)
-#%%
+table_create_q1 = """(artist text, song text, item_in_session int, length float, session_id int, 
+                        PRIMARY KEY (session_id, item_in_session))"""
+
 # ### 2. Give me only the following: name of artist, song (sorted by itemInSession) and user (first and last name) for userid = 10, sessionid = 182
-
-query = "CREATE TABLE IF NOT EXISTS music_library_q2 "
-query = (
-    query
-    + """(artist text, song text, first_name text, last_name text, item_in_session int, length float, level text, location text, session_id int, user_id int, 
-                PRIMARY KEY (user_id, session_id, item_in_session))"""
-)
-try:
-    session.execute(query)
-except Exception as e:
-    print(e)
-
-#%%
+table_create_q2 = """(artist text, song text, first_name text, last_name text, item_in_session int, length float, level text, location text, session_id int, user_id int, 
+                        PRIMARY KEY (user_id, session_id, item_in_session))"""
 
 # ### 3. Give me every user name (first and last) in my music app history who listened to the song 'All Hands Against His Own'
+table_create_q3 = """(song text, first_name text, last_name text, 
+                        PRIMARY KEY (song, first_name, last_name))"""
 
-query = """CREATE TABLE IF NOT EXISTS music_library_q3 """
-query = (
-    query
-    + """(song text, first_name text, last_name text, 
-                PRIMARY KEY (song, first_name, last_name))"""
+table_create_mapper = dict(
+    zip(
+        (table_q1, table_q2, table_q3),
+        (table_create_q1, table_create_q2, table_create_q3),
+    )
 )
-try:
-    session.execute(query)
-except Exception as e:
-    print(e)
+
+for table_name in table_create_mapper.keys():
+    create_table(table_name, session, table_create_mapper)
 #%%
-# map columns to order they appear in csv file line
-# columns from csv in order
-# 0 artist
-# 1 firstName
-# 2 gender
-# 3 itemInSession
-# 4 lastName
-# 5 length
-# 6 level
-# 7 location
-# 8 sessionId
-# 9 song
-# 10 userId
+
 col_map = {
     "artist": 0,
     "first_name": 1,
@@ -265,31 +88,6 @@ col_map = {
     "song": 9,
     "user_id": 10,
 }
-
-# In[1]:
-
-
-## TO-DO: Query 1:  Give me the artist, song title and song's length in the music app history that was heard during \
-## sessionId = 338, and itemInSession = 4
-
-
-# In[ ]:
-#%%
-import pandas as pd
-
-song_csv = pd.read_csv("event_datafile_new.csv")
-
-
-# We have provided part of the code to set up the CSV file. Please complete the Apache Cassandra code below#
-file = "event_datafile_new.csv"
-
-columns_to_insert = [
-    "artist",
-    "song",
-    "item_in_session",
-    "length",
-    "session_id",
-]
 
 col_name_file_map = {
     "artist": "artist",
@@ -306,53 +104,7 @@ col_name_file_map = {
 }
 
 
-# read with fixed chunksize = fixed memory usage
-df_songdata_chunks = pd.read_csv("event_datafile_new.csv", chunksize=100000)
-
-
-def get_value_from_csv(row, key: str):
-    return row._asdict().get(col_name_file_map.get(key))
-
-
-def insert_music_library_col(row, query_cols: tuple, table_name: str, session) -> None:
-    """Construct query to insert row of data from pandas itetuple object using
-    list of columns into the named table and execute it with the
-    given cassandra session
-
-    Args:
-            row:pandas object   row from pandas itertuple method
-            query_cols:tuple    list of column names to insert
-            table_name:str      name of the table to insert into
-            session             cassandra session object
-
-    Returns:
-            None
-
-    """
-    #  get elements in query_cols as one string for insertion into query
-    query_cols_asstring = ", ".join(query_cols)
-
-    #  compose query insert statement
-    query_insert = f"INSERT INTO {table_name} ({query_cols_asstring}) "
-
-    #  for each element in query cols create value substitution magics
-    subs = ", ".join(["%s" for x in query_cols])
-
-    #  compose value insertion query string
-    values = f"VALUES ({subs})"
-
-    #  get the data from row looking up the column names
-    cols = [get_value_from_csv(row, col_name) for col_name in query_cols]
-    #        try:
-
-    # execute the session, casting cols to tuple
-    session.execute(
-        f"{query_insert}{values}",
-        tuple(cols),
-    )
-
-
-#  ---------- DATA FOR QUERY CONSTRUCTION ---------------
+#  ---------- INSERT DATA FOR 3 QUERIES INTO DB  ---------------
 # Query 1 args:
 q1_cols = ("artist", "song", "item_in_session", "length", "session_id")
 q1_table_name = "music_library_q1"
@@ -376,132 +128,64 @@ q2_table_name = "music_library_q2"
 q3_cols = ("song", "first_name", "last_name")
 q3_table_name = "music_library_q3"
 
-#%%
+# read csv file with pandas - chunksize provides fixed mem use
+#  pandas will automatically cast dtypes to a suitable format
+file = "event_datafile_new.csv"
+df_songdata_chunks = pd.read_csv(file, chunksize=100000)
+
+# loop over the chunks using itertuples to preserve dtypes as described
+# in pandas docs for iterrows
+# run insert queries
 for chunk in df_songdata_chunks:
     for row in chunk.itertuples(index=False):
-        # Query 1"
-        insert_music_library_col(row, q1_cols, q1_table_name, session)
+        # Query 1
+        insert_music_library_col(
+            row, q1_cols, q1_table_name, session, col_name_file_map
+        )
 
-        # Query 2"
-        insert_music_library_col(row, q2_cols, q2_table_name, session)
+        # Query 2
+        insert_music_library_col(
+            row, q2_cols, q2_table_name, session, col_name_file_map
+        )
 
-        # Query 3"
-        insert_music_library_col(row, q3_cols, q3_table_name, session)
+        # Query 3
+        insert_music_library_col(
+            row, q3_cols, q3_table_name, session, col_name_file_map
+        )
+#%%
+# ------------- test 3 queries - will print to terminal ---------
+test_query_1 = (
+    "SELECT * FROM music_library_q1 WHERE session_id = 338 AND item_in_session = 4"
+)
+test_query_2 = "SELECT * FROM music_library_q2 WHERE user_id = 10 AND session_id = 182 ORDER BY item_in_session "
+test_query_3 = "SELECT first_name, last_name FROM music_library_q3 where song = 'All Hands Against His Own'"
 
+# run the test queries
+for query in [test_query_1, test_query_2, test_query_3]:
+    test_query(query, session)
 
 #%%
-# Test query 1
-query = "SELECT * FROM music_library_q1 WHERE session_id = 338 AND item_in_session = 4 "
-# try:
-rows = session.execute(query)
-# except Exception as e:
-#    print(e)
-for row in rows:
-    #    print(row.artist, row.song, row.item_in_session, row.length, row.session_id)
-    print(row)
-
-#%%
-# Test query 2
-# ### 2. Give me only the following: name of artist, song (sorted by itemInSession) and user (first and last name) for userid = 10, sessionid = 182
-query = "SELECT * FROM music_library_q2 WHERE user_id = 10 AND session_id = 182 ORDER BY item_in_session "
-# try:
-rows = session.execute(query)
-# except Exception as e:
-#    print(e)
-
-for row in rows:
-    [pp.pprint([k, v]) for k, v in row._asdict().items()]
-
-
-# Test query 3...
-#%%
-import pprint
-
-pp = pprint.PrettyPrinter(indent=4)
-
-
-#%%
-# Test query 3
-# ### 3. Give me every user name (first and last) in my music app history who listened to the song 'All Hands Against His Own'
-query = "SELECT first_name, last_name FROM music_library_q3 where song = 'All Hands Against His Own'"
+# --------------- drop the tables, close session -----------------------
+query = "drop table music_library_q1"
 try:
     rows = session.execute(query)
 except Exception as e:
     print(e)
-for row in rows:
-    rowdict []
-    
-    for column_name, value in row._asdict().items():
 
-        pp.pprint([column_name, value])
+query = "drop table music_library_q2"
+try:
+    rows = session.execute(query)
+except Exception as e:
+    print(e)
 
-#%%
-with open(file, encoding="utf8") as f:
-    csvreader = csv.reader(f)
-    next(csvreader)  # skip header
-
-    for line in csvreader:
-        query = "INSERT INTO music_library_q1 (artist, song, item_in_session, length, session_id) "
-        query = query + "VALUES (%s, %s, %s, %s, %s)"
-        cols = ["artist", "song", "item_in_session", "length", "session_id"]
-
-        #        try:
-        session.execute(
-            query,
-            tuple([line[col_map.get(col)] for col in cols]),
-        )
-#        except Exception as e:
-#            print(e)
-## TO-DO: Assign which column element should be assigned for each column in the INSERT statement.
-## For e.g., to INSERT artist_name and user first_name, you would change the code below to `line[0], line[1]`
+query = "drop table music_library_q3"
+try:
+    rows = session.execute(query)
+except Exception as e:
+    print(e)
 
 
-# #### Do a SELECT to verify that the data have been inserted into each table
-
-# In[ ]:
-
-## TO-DO: Add in the SELECT statement to verify the data was entered into the table
-#%%
-
-# ### COPY AND REPEAT THE ABOVE THREE CELLS FOR EACH OF THE THREE QUESTIONS
-
-# In[ ]:
-
-
-## TO-DO: Query 2: Give me only the following: name of artist, song (sorted by itemInSession) and user (first and last name)\
-## for userid = 10, sessionid = 182
-
-
-# In[ ]:
-
-
-## TO-DO: Query 3: Give me every user name (first and last) in my music app history who listened to the song 'All Hands Against His Own'
-
-
-# In[ ]:
-
-
-# In[ ]:
-
-
-# ### Drop the tables before closing out the sessions
-
-# In[4]:
-
-
-## TO-DO: Drop the table before closing out the sessions
-
-
-# In[ ]:
-
-
-# ### Close the session and cluster connection¶
-
-# In[ ]:
-
-
-# session.shutdown()
-# cluster.shutdown()
-
-
+#  close session and cluster
+session.shutdown()
+cluster.shutdown()
 #%%
